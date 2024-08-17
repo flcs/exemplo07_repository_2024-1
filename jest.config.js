2cec3adcee3dd46214425e277345b993fc3f18b1
Fernando Carvalho <f.carvalho@gsuite.iff.edu.br>
2024-08-17 07:35:04 -0300

    ignorando testes no /dist

--- jest.config.js
+++ jest.config.js
@@ -92,7 +92,9 @@ const config = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+  ],
 
   // Activates notifications for test results
   // notify: false,
